1eff2e3782a957a5d2ba8b581ac55165c89577b1
Extremekrish <help.jokem@gmail.com>
2026-09-10 02:54:32 +0530

    update

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -259,11 +259,13 @@ export function renderPage(
 
   const lang = componentData.fileData.frontmatter?.lang ?? cfg.locale?.split("-")[0] ?? "en"
   const direction = i18n(cfg.locale).direction ?? "ltr"
+  //Updated Line Below
+  const frontmatterBodyClass = componentData.fileData.frontmatter?.body_class as string || ""
   const doc = (
     <html lang={lang} dir={direction}>
       <Head {...componentData} />
-      <body data-slug={slug}>
-        <div id="quartz-root" class="page">
+      <body data-slug={slug} class={frontmatterBodyClass}>
+        <div id="quartz-root" class={`page ${frontmatterBodyClass}`}>
           <Body {...componentData}>
             {LeftComponent}
             <div class="center">
