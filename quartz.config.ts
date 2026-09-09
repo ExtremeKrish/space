33d5699fef5c471629c006a4332cae3b4235d672
Extremekrish <help.jokem@gmail.com>
2026-09-09 15:03:22 +0530

    update

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Newsreader",
-        body: "Newsreader",
+        header: "Inter",
+        body: "Inter",
         code: "IBM Plex Mono",
       },
       colors: {
